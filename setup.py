9282ee501c567876ed0f5e9e4338c14ca16a431f
admiralobvious <aferlandqc@gmail.com>
2018-08-31 10:09:36 -0400

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vyper-config',
-    version='0.2.2',
+    version='0.2.3',
     description='Python configuration with more fangs',
     url='http://github.com/admiralobvious/vyper',
     author='Alexandre Ferland',
