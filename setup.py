6fa801735ae1cf062a33ed34ad07c81a81a7019a
Alexandre Ferland <alf@pixmob.com>
2019-11-07 11:52:56 -0500

    drop Py 3.5

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
